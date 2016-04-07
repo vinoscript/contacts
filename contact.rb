01347bee12e1a1caafb0995026f137e0dbb6c7a9
Willemina SB <wstikk@gmail.com>
2016-04-06 19:01:00 -0700

    Refactored some methods

--- contact.rb
+++ contact.rb
@@ -8,11 +8,7 @@ class Contact
   # @@contacts = []
   attr_accessor :name, :email, :id
   
-  # Creates a new contact object
-  # @param name [String] The contact's name
-  # @param email [String] The contact's email address
   def initialize(name, email, num)
-    # TODO: Assign parameter values to instance variables.
     @name = name
     @email = email
     @id = num
@@ -30,14 +26,9 @@ class Contact
     # @return [Array<Contact>] Array of Contact objects
     def all
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
-      i=1
-      contacts = []
-      CSV.foreach(FILE) do |row|
-        # Contact.new(row[0], row[1], row[2])
-        contacts << Contact.new(row[0], row[1], i)
-        i+=1
+      CSV.read(FILE).each_with_index.map do |row, i|
+        Contact.new(row[0], row[1], i+1)
       end
-      contacts
     end
 
     # Creates a new contact, adding it to the csv file, returning the new contact.
@@ -45,32 +36,16 @@ class Contact
     # @param email [String] the contact's email
     def create(name, email)
       # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
-      # @@contacts = []
-      # @@contacts = Contact.all
-
-      len = @@contacts.length
-      puts "Length of contacts array is #{len}"
-      CSV.open(FILE, 'ab') do |csv|
-        # new_id = CSV.read(FILE).last[2].to_i + 1
-        csv << [name, email]
-      end
-      Contact.new(name,email,len+1)
+      CSV.open(FILE, 'a') << [name, email]
+      Contact.new(name,email,Contact.all.length + 1)
     end
     
     # Find the Contact in the 'contacts.csv' file with the matching id.
     # @param id [Integer] the contact id
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
-      # @@contacts = []
-      # @@contacts = Contact.all
-
       Contact.all.select do |contact|
-        if contact.id == id
-          return contact
-        else
-          nil
-        end
+        contact.id == id
       end
     end
     
@@ -79,16 +54,8 @@ class Contact
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
       # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
-      # @@contacts = []
-      # @@contacts = Contact.all
-
-      # Contact.all.select do |contact|
       Contact.all.select do |contact|
-        if contact.name.downcase.match(term) || contact.email.downcase.match(term)
-          contact
-        else
-          nil
-        end
+        contact.name.downcase.match(term) || contact.email.downcase.match(term)
       end
     end
 
